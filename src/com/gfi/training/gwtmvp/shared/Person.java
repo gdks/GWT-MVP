6bf17e28fe25a9b92425a96f0f6155e66781162e
Gavin Stewart <gavin@gdks.co.uk>
2014-06-05 16:29:23 +0100

    initial

---
+++ src/com/gfi/training/gwtmvp/shared/Person.java
@@ -0,0 +1,31 @@
+package com.gfi.training.gwtmvp.shared;
+
+public class Person {
+	protected String firstname;
+	protected String lastname;
+	
+	public Person() {
+		this("Gavin", "Stewart");
+	}
+	
+	public Person(String firstname, String lastname) {
+		this.firstname = firstname;
+		this.lastname = lastname;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+}
