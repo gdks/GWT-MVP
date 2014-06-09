fa84da95482ee70eb0b5ca3e719e1d3abcbc02b4
Gavin Stewart <gavin@gdks.co.uk>
2014-06-09 12:50:46 +0100

    remove presenters

--- src/com/gfi/training/gwtmvp/client/presenters/Presenter.java
+++
@@ -1,8 +0,0 @@
-package com.gfi.training.gwtmvp.client.presenters;
-
-import com.google.gwt.user.client.ui.Panel;
-
-public interface Presenter {
-	public void bind();
-	public void go(Panel panel);
-}
