af605e369be971ea6526e14b5006300fa2ec3e9a
Gavin Stewart <gavin@gdks.co.uk>
2014-06-13 16:04:00 +0100

    BoardTokenizerTest

--- junit-src/com/gfi/training/gwtmvp/client/places/BoardPlaceTokenizerTest.java
+++ junit-src/com/gfi/training/gwtmvp/client/places/BoardPlaceTokenizerTest.java
@@ -1,28 +1,28 @@
 package com.gfi.training.gwtmvp.client.places;
 
-import static org.mockito.Mockito.verify;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
 
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.user.client.ui.Label;
 import com.googlecode.gwt.test.GwtModule;
 import com.googlecode.gwt.test.GwtTestWithMockito;
 
 @GwtModule("com.gfi.training.gwtmvp.mvp")
 public class BoardPlaceTokenizerTest extends GwtTestWithMockito {
 	
-	private BoardPlaceTokenizerTest testSubject;
-	
-	@Before
-	public void setUp() throws Exception {
-	}
-
 	@Test
-	public void test() {
+	public void testGetPlace() {
+		BoardPlace.Tokenizer testSubject = new BoardPlace.Tokenizer();
+		BoardPlace result = testSubject.getPlace("Gavin Stewart");
 		
+		assertEquals("Gavin Stewart", result.getToken());
+	}
+	
+	@Test
+	public void testGetToken() {
+		BoardPlace.Tokenizer testSubject = new BoardPlace.Tokenizer();
+		String result = testSubject.getToken(new BoardPlace("Gavin Stewart"));
+		assertEquals("Gavin Stewart", result);
 	}
 
 }
