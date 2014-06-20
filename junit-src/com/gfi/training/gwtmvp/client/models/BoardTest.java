f454d8db60839238f75c5b4f2f6292c033640bce
Gavin Stewart <gavin@gdks.co.uk>
2014-06-20 09:41:00 +0100

    Add board model

---
+++ junit-src/com/gfi/training/gwtmvp/client/models/BoardTest.java
@@ -0,0 +1,60 @@
+package com.gfi.training.gwtmvp.client.models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BoardTest {
+	
+	private Board testSubject;
+	
+	@Before
+	public void setUp() throws Exception {
+		testSubject = new Board();
+	}
+	
+	@Test
+	public void testPlacingAPieceOnTheBoard() {
+		testSubject.put(Board.Square.THREE, Player.CROSS);
+	}
+	
+	@Test
+	public void testIfBoardReturnsFullWhenAllSquaresPlayed() {
+	  
+		testSubject.put(Board.Square.ONE, Player.NAUGHT);
+		testSubject.put(Board.Square.TWO, Player.CROSS);
+		testSubject.put(Board.Square.THREE, Player.NAUGHT);
+		testSubject.put(Board.Square.FOUR, Player.CROSS);
+		testSubject.put(Board.Square.FIVE, Player.NAUGHT);
+		testSubject.put(Board.Square.SIX, Player.CROSS);
+		testSubject.put(Board.Square.SEVEN, Player.NAUGHT);
+		testSubject.put(Board.Square.EIGHT, Player.CROSS);
+		testSubject.put(Board.Square.NINE, Player.NAUGHT);
+		
+		assertTrue(testSubject.isFull());
+	}
+	
+	@Test
+	public void testIfBoardReturnsFullWhenThereAreEmptySquares() {
+	  
+		testSubject.put(Board.Square.ONE, Player.NAUGHT);
+		testSubject.put(Board.Square.TWO, Player.CROSS);
+		testSubject.put(Board.Square.THREE, Player.NAUGHT);
+		testSubject.put(Board.Square.EIGHT, Player.CROSS);
+		testSubject.put(Board.Square.NINE, Player.NAUGHT);
+		
+		assertFalse(testSubject.isFull());
+	}
+	
+	@Test
+	public void testIfPopulatedSquareReturnsAPlayer() {
+		testSubject.put(Board.Square.ONE, Player.NAUGHT);
+		assertEquals(Player.NAUGHT, testSubject.getPlayerOnSquare(Board.Square.ONE));
+	}
+	
+	@Test
+	public void testIfEmptySquareReturnsAPlayer() {
+		assertEquals(null, testSubject.getPlayerOnSquare(Board.Square.ONE));
+	}
+}
---
+++ src/com/gfi/training/gwtmvp/client/models/Board.java
@@ -0,0 +1,52 @@
+package com.gfi.training.gwtmvp.client.models;
+
+import com.google.gwt.dev.util.collect.HashMap;
+
+public class Board {
+	
+	private HashMap<Board.Square, Player> data;
+	
+	public Board() {
+		initBoard();
+	}
+	
+	public void initBoard() {
+		
+		data = new HashMap<Board.Square, Player>();
+	}
+	
+	protected boolean isFull() {
+		
+		if(data.size() == 9) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public void put(Board.Square square, Player player) {
+		data.put(square, player);
+	}
+
+	public Player getPlayerOnSquare(Board.Square square) {
+		
+		Player value = data.get(square);
+		
+		if (value != null) {
+			return value;
+		} else {
+			return null;
+		}
+	}
+	
+	enum Square {
+		ONE(1),TWO(2),THREE(3),FOUR(4),FIVE(5),SIX(6),SEVEN(7),EIGHT(8),NINE(9);
+		
+		@SuppressWarnings("unused")
+		private int value;
+
+	    private Square(int value) {
+	    	this.value = value;
+	    }
+	}
+}
