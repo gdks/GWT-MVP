87de1e27250d61142e2f1dca01d45d245b113484
Gavin Stewart <gavin@gdks.co.uk>
2014-07-04 13:52:45 +0100

    Update Board.Square enum to be public

--- src/com/gfi/training/gwtmvp/shared/Board.java
+++ src/com/gfi/training/gwtmvp/shared/Board.java
@@ -39,7 +39,7 @@ public class Board {
 		}
 	}
 	
-	enum Square {
+	public enum Square {
 		ONE(1),TWO(2),THREE(3),FOUR(4),FIVE(5),SIX(6),SEVEN(7),EIGHT(8),NINE(9);
 		
 		@SuppressWarnings("unused")
